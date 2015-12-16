c6a5e2b3a86785efb2e48d932005e8f5169fbe21
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-12-16 18:49:02 +0900

    Init

---
+++ Assets/Packages/Mapper/Mapper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MapperSystem {
+	[RequireComponent(typeof(Camera))]
+	public class Mapper : MonoBehaviour {
+		public Shader mapperShader;
+	}
+}
