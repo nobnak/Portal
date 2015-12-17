78ab7d31557e36a789c56127104837fcdb579c3f
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-12-17 16:52:44 +0900

    no message

--- Assets/Packages/Portal/Portal.cs
+++ Assets/Packages/Portal/Portal.cs
@@ -3,33 +3,30 @@ using System.Collections;
 using System.Text;
 
 namespace PortalSystem {
-	[ExecuteInEditMode]
 	public class Portal : MonoBehaviour {
 		public Camera targetCamera;
 		public Portal pair;
 
+		MeshFilter _filt;
 		Mesh _mesh;
 		Vector3[] _vertices;
 		Vector2[] _uvs;
 
 		void OnEnable() {
-			var filt = GetComponent<MeshFilter>();
-			_mesh = Instantiate(filt.sharedMesh);
+			_filt = GetComponent<MeshFilter>();
+			_mesh = _filt.mesh;
 			_mesh.MarkDynamic();
 			_vertices = _mesh.vertices;
 			_uvs = _mesh.uv;
 		}
 		void OnDisable() {
-			if (Application.isPlaying)
-				Destroy(_mesh);
-			else
-				DestroyImmediate(_mesh);
+			Destroy(_mesh);
 		}
 		void Update() {
 			if (targetCamera == null || pair == null)
 				return;
 
-			Connect(transform.localToWorldMatrix, targetCamera, _vertices);			
+			pair.Connect(transform.localToWorldMatrix, targetCamera, _vertices);			
 		}
 
 		void Connect(Matrix4x4 pairModel, Camera cam, Vector3[] pairVertices) {
@@ -37,11 +34,6 @@ namespace PortalSystem {
 			for (var i = 0; i < vertexCount; i++)
 				_uvs[i] = (Vector2)cam.WorldToViewportPoint(pairModel.MultiplyPoint3x4(pairVertices[i]));
 			_mesh.uv = _uvs;
-
-			var buf = new StringBuilder("UV :");
-			for (var i = 0; i < _uvs.Length; i++)
-				buf.AppendFormat(" <{0:f2},{1:f2}>", _uvs[i].x, _uvs[i].y);
-			Debug.LogFormat("Update UV : {0}", buf);
 		}
 	}
 }
